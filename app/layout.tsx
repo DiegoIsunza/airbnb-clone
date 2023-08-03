670419e19abf78ccf1728a87af5281f83c24d685
DiegoIsunza <contacto.diegoisunza@gmail.com>
2023-08-02 23:42:10 -0600

    register, prisma and mongo implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Nunito } from 'next/font/google';
 
 import './globals.css'
 import Navbar from './components/navbar/Navbar';
+import RegisterModal from './components/modals/RegisterModal';
+import ToasterProvider from './providers/ToasterProvider';
 
 export const metadata: Metadata = {
   title: 'Airbnb',
@@ -20,8 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
-        {children}
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
       </body>
     </html>
   )
